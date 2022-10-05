00f8fe303ce66a2af27c0b0b97c1098b099ed5dd
Jdlabitad <114962476+Jdlabitad@users.noreply.github.com>
2022-10-05 14:43:05 +0800

    Add files via upload

---
+++ data/geotag_3.js
@@ -0,0 +1,1 @@
+var json_geotag_3 = {"type":"FeatureCollection","name":"geotag_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"name":"Don Narciso","folder":null},"geometry":{"type":"Point","coordinates":[124.967806599538235,8.642232887398224]}}]}
